994f990d2362552889040f9011463c665d556358
fionatdesign <ft98@drexel.edu>
2022-04-13 17:13:45 -0400

    adjust greensock animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,14 +2,8 @@ import { gsap } from "gsap";
 
 gsap.to("#aldente", {rotation: 360, transformOrigin: "center", ease: "none", duration: 30, repeat: -1});
 
-gsap.from("#hero-content", {duration: 2.5, ease: "power1.out", y: -100});
+gsap.from("#hero-content", {delay: 1, duration: 3, opacity: 0});
 
 //gsap.to("#penne-pasta", {rotation: 45, transformOrigin: "center", ease: "none", duration: 30, repeat: 2});
 
-gsap.from("#penne-pasta", {duration: 2, rotation: 45, ease: "bounce"});
-
-gsap.from('#bowtie-pasta', 1, {
-    duration: 2, rotation: -45, ease: "bounce"}
-    );
-
-    
+//gsap.from("#penne-pasta", {duration: 2, rotation: 45, ease: "bounce"});
