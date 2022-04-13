eb848e8489eba66d0637d9a1c03e6e2cfabc71bb
fionatdesign <ft98@drexel.edu>
2022-04-13 16:39:21 -0400

    adjusted hero sticker section

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,4 +4,12 @@ gsap.to("#aldente", {rotation: 360, transformOrigin: "center", ease: "none", dur
 
 gsap.from("#hero-content", {duration: 2.5, ease: "power1.out", y: -100});
 
-//gsap.to("#bowtie-pasta", {});
+//gsap.to("#penne-pasta", {rotation: 45, transformOrigin: "center", ease: "none", duration: 30, repeat: 2});
+
+gsap.from("#penne-pasta", {duration: 2, rotation: 45, ease: "bounce"});
+
+gsap.from('#bowtie-pasta', 1, {
+    duration: 2, rotation: -45, ease: "bounce"}
+    );
+
+    
