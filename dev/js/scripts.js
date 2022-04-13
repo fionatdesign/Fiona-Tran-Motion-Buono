6a90314d624ecc366a86c9d33fefd1fc4276b086
fionatdesign <ft98@drexel.edu>
2022-04-13 15:23:25 -0400

    adjusted hero scss

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,7 @@
-//import { gsap } from "gsap";
+import { gsap } from "gsap";
+
+//gsap.to("#aldente", {rotation: 360, transformOrigin: "center", ease: "none", duration: 60, repeat: -1});
+
+gsap.from("#hero-content", {duration: 2.5, ease: "power1.out", y: -100});
+
+gsap.to("#bowtie-pasta", {wiggles:wiggles});
