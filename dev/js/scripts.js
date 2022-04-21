eaec5e975f06b0c473c964236df35bfef2b3b131
fionatdesign <ft98@drexel.edu>
2022-04-21 16:43:29 -0400

    adjusted hero animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,12 +1,19 @@
 import { gsap } from "gsap";
 
-gsap.from("#hero-content", {delay: 1, duration: 3, opacity: 0});
+// gsap.from("#hero-content", {delay: 1, duration: 3, opacity: 0});
 
-gsap.to("#aldente" , {rotation: 360, transformOrigin: "center", ease: "none", duration: 30, repeat: -1});
+// gsap.to("#aldente" , {rotation: 360, transformOrigin: "center", ease: "none", duration: 30, repeat: -1});
 
-gsap.from("#penne-pasta", {duration: 1, rotation: -15, ease: "bounce.out"});
+// gsap.from("#penne-pasta", {duration: 1, rotation: -15, ease: "bounce.out"});
 
-gsap.from("#bowtie-pasta", {duration: 1, rotation: 15, ease: "bounce.out"});
+// gsap.from("#bowtie-pasta", {duration: 1, rotation: 15, ease: "bounce.out"});
 
+const mainTL = gsap.timeline();
 
-//gsap.fromTo("#penne-pasta", {rotation: 30, transformOrigin: "center", ease: "none", duration: 1, repeat: -1}, {rotation:-30, transformOrigin: "center", ease: "none", duration: 1, reversed: 1});
+mainTL.from(".pastahero-img", {x:-500, duration: 3, ease: "power3.out"}, "heroimg")
+.from("#hero-content", {duration: 3, opacity:0, y:50, delay: 1}, "heroimg+=2")
+.from("#aldente", {x: 800, duration:3, rotation:360, ease: "power3.out"}, "animate")
+.from("#pennesticker", {x:800, duration: 3, rotation:360, ease: "power3.out"}, "animate+=.75")
+.from("#bowtiesticker" , {x:850, duration: 3, rotation: 360, ease: "power3.out"}, "animate+=1.5")
+
+;
